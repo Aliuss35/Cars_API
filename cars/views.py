bd94303d747d36f80f74840cab43f9cda4e2cb58
ali <aliyavuz2000@hotmail.com>
2022-07-13 07:50:03 -0400

    updated urls

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+#allows function to handle get post patch/put delete
+from rest_framework.response import Response
+
+#this is how we add decorators
+@api_view(['GET'])
+def cars_list(request):
+
+
+  return Response('ok')#response from API to html. 
 
 # Create your views here.
