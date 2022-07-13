6002b653e265744c7eb4ed8cfbcf6f8457ed34c6
ali <aliyavuz2000@hotmail.com>
2022-07-13 13:11:34 -0400

    added 2 models through adming page

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,17 @@
 from rest_framework.decorators import api_view
 #allows function to handle get post patch/put delete
 from rest_framework.response import Response
+from .models import Car
+from .serializers import CarSerializer
+from cars import serializers
 
 #this is how we add decorators
 @api_view(['GET'])
 def cars_list(request):
+  cars = Car.objects.all()
 
+  serializer =CarSerializer(cars, many=True)
 
-  return Response('ok')#response from API to html. 
+  return Response(serializer.data)#response from API to html. 
 
 # Create your views here.
