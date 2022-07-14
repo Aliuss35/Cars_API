132763aea470df72ac3a85f98f30ab9533caf630
ali <aliyavuz2000@hotmail.com>
2022-07-14 11:14:54 -0400

    changed extra import

--- cars/views.py
+++ cars/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from .models import Car
 from .serializers import CarSerializer
-from cars import serializers
+
 
 
 #this is how we add decorators
